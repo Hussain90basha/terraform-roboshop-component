8f6fd9175f9e9c2ee0746c07e3345f03c4d0c584
Hussain90basha <bashahussain9010@gmail.com>
2025-12-03 18:38:41 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -5,12 +5,12 @@ locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     ami_id = data.aws_ami.joindevops.id
     sg_id = data.aws_ssm_parameter.sg_id.value
-    tg_port = ${var.component} == "frontend" ? 80 : 8080
-    health_check_path = ${var.component} == "frontend" ? "/" : "/health"
+    tg_port = "${var.component}" == "frontend" ? 80 : 8080
+    health_check_path = "${var.component}" == "frontend" ? "/" : "/health"
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-    listener_arn = ${var.component} == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
-    host_context = ${var.component} == "frontend" ? "${var.project_name}-${var.environment}.${domain_name}" : "${var.component}.backend_alb-${var.environment}.${var.domain_name}"
+    listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+    host_context = "${var.component}" == "frontend" ? "${var.project_name}-${var.environment}.${domain_name}" : "${var.component}.backend_alb-${var.environment}.${var.domain_name}"
     common_tags = {
         Project = var.project_name
         Environment = var.environment
